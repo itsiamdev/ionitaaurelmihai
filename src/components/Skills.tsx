48e92c0fe4aff5d17d14bcf9ab56b3c48483bdf0
Aurel Ionita <ionitaaurel32@gmail.com>
2026-04-05 21:32:58 +0300

    feat: add TypeScript to skills list in Skills component

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -9,7 +9,8 @@ const Skills = () => {
         { name: "C", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" },
         { name: "C++", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" },
         { name: "C#", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" },
-        { name: "JavaScript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" }
+        { name: "JavaScript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+        { name: "TypeScript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" }
       ],
     },
     {
